28a573f8b0afbe4b3957a2924078a8e08cd5b9fd
SAKSHAM KAMBOJ <sakshamkamboj2072@gmail.com>
2024-02-13 23:02:00 +0530

    finish

--- server/app.js
+++ server/app.js
@@ -18,10 +18,10 @@ app.use(require("./routes/user"))
 
 connectDB();
 
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'),
+  res.sendFile(path.join(__dirname, '../client/build/index.html'),
     function (err) {
       res.status(500).send(err);
     }
